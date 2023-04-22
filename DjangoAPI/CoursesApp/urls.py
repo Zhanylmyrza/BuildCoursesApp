5189545c4fed9ea345004beceb45381f82291de5
Zhanylmyrza <93324684+Zhanylmyrza@users.noreply.github.com>
2023-04-22 17:16:01 +0600

    Update urls.py

--- DjangoAPI/CoursesApp/urls.py
+++ DjangoAPI/CoursesApp/urls.py
@@ -1,10 +1,10 @@
 from django.urls import  path
 from CoursesApp import views
-from CoursesApp.views import courseApi
-from CoursesApp.views import courseDetailView
+from CoursesApp.views import CourseApi
+from CoursesApp.views import CourseDetailView
 
 urlpatterns = [
-    path("course", views.courseApi.as_view()),
-    path("course/<int:id>",courseDetailView.as_view()),
+    path("course", views.CourseApi.as_view()),
+    path("course/<int:id>",CourseDetailView.as_view()),
 ]
 
